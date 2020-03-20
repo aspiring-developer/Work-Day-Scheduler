728f727af4a31dad75268b20d1738558b2b791af
Shi Sha <emailtotheowner@gmail.com>
2020-03-20 17:56:43 -0400

    added modified files.

---
+++ styles-script.js
@@ -0,0 +1,161 @@
+// Getting DOM element references to style main header, clock display and footer
+var currentDayEl = document.getElementById('currentDay');
+var jumbotronHeaderEl = document.getElementById('jumbotronHeader');
+var footerEl = document.getElementById('footerDiv');
+// Styling main header, clock display and footer
+currentDayEl.setAttribute(
+'style',
+'text-transform: uppercase; font-weight: bold; font-size: 1.5rem; color: #1dbec4'
+);
+jumbotronHeaderEl.setAttribute(
+'style',
+'text-transform: uppercase; font-weight: bold; font-size: 4rem; text-shadow: 2px 2px 8px #4f7c5e; color: #0f394f'
+);
+// footerEl.setAttribute =
+// ('style', 'justify-content: center; align-items: center; color: red; text-align: center');
+
+// var allLeftDivEl = document.querySelectorAll(".leftDiv");
+// allLeftDivEl.setAttribute('style', 'height:50px; background-color:#fafafa; width:15%');
+
+timeBlockDivOneOne.setAttribute(
+  'style',
+  'height:50px; background-color:#fafafa; width:15%; border-top-left-radius:10px; border-bottom-left-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivOneOne.innerHTML = '9:00AM';
+timeBlockDivOneTwo.setAttribute(
+  'style',
+  'height:50px; background-color:#dadada; width:70%; border: 1px solid #a0b7db; float: left'
+);
+timeBlockDivOneThree.setAttribute(
+  'style',
+  'height:50px; background-color:#1dbec4; color: white; width:15%; border-top-right-radius:10px; border-bottom-right-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivOneThree.innerHTML = '<i class="fas fa-lock"></i>';
+
+// ===== Styling three divs in row-2 ===== //
+timeBlockDivTwoOne.setAttribute(
+  'style',
+  'height:50px; background-color:#fafafa; width:15%; border-top-left-radius:10px; border-bottom-left-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivTwoOne.innerHTML = '10:00AM';
+timeBlockDivTwoTwo.setAttribute(
+  'style',
+  'height:50px; background-color:#dadada; width:70%; border: 1px solid #a0b7db; float: left'
+);
+timeBlockDivTwoThree.setAttribute(
+  'style',
+  'height:50px; background-color:#1dbec4; color: white; width:15%; border-top-right-radius:10px; border-bottom-right-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivTwoThree.innerHTML = '<i class="fas fa-lock"></i>';
+
+// ===== Styling three divs in row-3 ===== //
+timeBlockDivThreeOne.setAttribute(
+  'style',
+  'height:50px; background-color:#fafafa; width:15%; border-top-left-radius:10px; border-bottom-left-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivThreeOne.innerHTML = '11:00AM';
+timeBlockDivThreeTwo.setAttribute(
+  'style',
+  'height:50px; background-color:#dadada; width:70%; border: 1px solid #a0b7db; float: left'
+);
+timeBlockDivThreeThree.setAttribute(
+  'style',
+  'height:50px; background-color:#1dbec4; color: white; width:15%; border-top-right-radius:10px; border-bottom-right-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivThreeThree.innerHTML = '<i class="fas fa-lock"></i>';
+
+// ===== Styling three divs in row-4 ===== //
+timeBlockDivFourOne.setAttribute(
+  'style',
+  'height:50px; background-color:#fafafa; width:15%; border-top-left-radius:10px; border-bottom-left-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivFourOne.innerHTML = '12:00PM';
+timeBlockDivFourTwo.setAttribute(
+  'style',
+  'height:50px; background-color:#dadada; width:70%; border: 1px solid #a0b7db; float: left'
+);
+timeBlockDivFourThree.setAttribute(
+  'style',
+  'height:50px; background-color:#1dbec4; color: white; width:15%; border-top-right-radius:10px; border-bottom-right-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivFourThree.innerHTML = '<i class="fas fa-lock"></i>';
+
+// ===== Styling three divs in row-5 ===== //
+timeBlockDivFiveOne.setAttribute(
+  'style',
+  'height:50px; background-color:#fafafa; width:15%; border-top-left-radius:10px; border-bottom-left-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivFiveOne.innerHTML = '1:00PM';
+timeBlockDivFiveTwo.setAttribute(
+  'style',
+  'height:50px; background-color:#dadada; width:70%; border: 1px solid #a0b7db; float: left'
+);
+timeBlockDivFiveThree.setAttribute(
+  'style',
+  'height:50px; background-color:#1dbec4; color: white; width:15%; border-top-right-radius:10px; border-bottom-right-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivFiveThree.innerHTML = '<i class="fas fa-lock"></i>';
+
+// ===== Styling three divs in row-6 ===== //
+timeBlockDivSixOne.setAttribute(
+  'style',
+  'height:50px; background-color:#fafafa; width:15%; border-top-left-radius:10px; border-bottom-left-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivSixOne.innerHTML = '2:00PM';
+timeBlockDivSixTwo.setAttribute(
+  'style',
+  'height:50px; background-color:#dadada; width:70%; border: 1px solid #a0b7db; float: left'
+);
+timeBlockDivSixThree.setAttribute(
+  'style',
+  'height:50px; background-color:#1dbec4; color: white; width:15%; border-top-right-radius:10px; border-bottom-right-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivSixThree.innerHTML = '<i class="fas fa-lock"></i>';
+
+// ===== Styling three divs in row-7 ===== //
+timeBlockDivSevenOne.setAttribute(
+  'style',
+  'height:50px; background-color:#fafafa; width:15%; border-top-left-radius:10px; border-bottom-left-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivSevenOne.innerHTML = '3:00PM';
+timeBlockDivSevenTwo.setAttribute(
+  'style',
+  'height:50px; background-color:#dadada; width:70%; border: 1px solid #a0b7db; float: left'
+);
+timeBlockDivSevenThree.setAttribute(
+  'style',
+  'height:50px; background-color:#1dbec4; color: white; width:15%; border-top-right-radius:10px; border-bottom-right-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivSevenThree.innerHTML = '<i class="fas fa-lock"></i>';
+
+// ===== Styling three divs in row-8 ===== //
+timeBlockDivEightOne.setAttribute(
+  'style',
+  'height:50px; background-color:#fafafa; width:15%; border-top-left-radius:10px; border-bottom-left-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivEightOne.innerHTML = '4:00PM';
+timeBlockDivEightTwo.setAttribute(
+  'style',
+  'height:50px; background-color:#dadada; width:70%; border: 1px solid #a0b7db; float: left'
+);
+timeBlockDivEightThree.setAttribute(
+  'style',
+  'height:50px; background-color:#1dbec4; color: white; width:15%; border-top-right-radius:10px; border-bottom-right-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivEightThree.innerHTML = '<i class="fas fa-lock"></i>';
+
+// ===== Styling three divs in row-9 ===== //
+timeBlockDivNineOne.setAttribute(
+  'style',
+  'height:50px; background-color:#fafafa; width:15%; border-top-left-radius:10px; border-bottom-left-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: left'
+);
+timeBlockDivNineOne.innerHTML = '5:00PM';
+timeBlockDivNineTwo.setAttribute(
+  'style',
+  'height:50px; background-color:#dadada; width:70%; border: 1px solid #a0b7db; float: left'
+);
+timeBlockDivNineThree.setAttribute(
+  'style',
+  'height:50px; background-color:#1dbec4; color: white; width:15%; border-top-right-radius:10px; border-bottom-right-radius:10px; display: flex; justify-content:center; align-items:center; border:1px solid #a0b7db; float: none'
+);
+timeBlockDivNineThree.innerHTML = '<i class="fas fa-lock"></i>';
