a4f685f3e37daf84f0b4b81034fe80eb35694018
Shi Sha <emailtotheowner@gmail.com>
2020-03-22 17:15:08 -0400

    Combined and updated multi-working files making one working file at this stage.

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+	// This function is a "wrapper function" for jQuery use //
+	// ########## TIMER - 1: ########## Placing a clock in the jumbotron header section
+	const dayMom = moment();
+	function update() {
+		$('#currentDay').html(moment().format('dddd, D MMMM YYYY, H:mm:ss'));
+	}
+	setInterval(update, 1000);
+
+	// ########## TIMER - 2: ########## Times:left div, Changes: mid div, Saves: right div
+		var currentTime = moment().format('H');
+	// console.log(currentTime); // Prints the current time in hour
+// ===== Looping through 9am to 17pm ===== //
+	for (i = 9; i < 18; i++) {
+		makeRow(i);
+		var divEl = document.getElementById('midDiv' + i);
+		console.log('midDiv' + i); // Prints total 9 to 17 midDiv values
+		if (currentTime == i) {
+			divEl.style.backgroundColor = '#FF6961';
+		} else if (currentTime > 18) {
+			divEl.style.backgroundColor = '#F6E5F5';
+		} else {
+			divEl.style.backgroundColor = '#77DD77';
+		}
+	}
+
+	// ########## Creating rows for 9:00AM-17:00PM, and middle input field ##########
+	function makeRow(i) {
+		// Targeting timeBlock div DOM element to create rows
+		var timeBlockEl = document.getElementById('timeBlock');
+		// Creating a div to make rows and add to the timeBlock div DOM element
+		var rowEl = document.createElement('div');
+		// Adding the div to the existing timeBlock div
+		timeBlockEl.appendChild(rowEl);
+		// Using template literals to format the rows
+		rowEl.innerHTML = `	
+		<div id="leftDiv" class="leftDivs"> 
+		   ${i > 12 ? i + ':00PM' : i + ':00AM'} 
+		</div>
+		<div id="midDiv${i}" class="midDivs">
+		   <input id="inputField${i}" class="inputBoxes">
+		</div>
+			`;
+
+		// ########## Creating save buttons column and adding event listeners to them ##########
+		var saveButton = document.createElement('div');
+		saveButton.innerHTML = `
+		<div class="rightDiv"> 
+		<i class="fas fa-save" value='${i}'></i>
+		</div>
+		`;
+		
+		saveButton.addEventListener('click', function(e) {
+			//console.log(e.target.getAttribute('value'));
+					var inputValue = document.getElementById('inputField' + i).value;
+			});
+		rowEl.appendChild(saveButton);
+	}
+
+	// ====================================================================
+}); // "wrapper function" ending
